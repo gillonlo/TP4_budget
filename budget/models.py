6cce3d09eefdd58066bb37186dbe9c4c3fc06cc0
gillonlo <74982380+gillonlo@users.noreply.github.com>
2022-10-31 17:05:53 -0400

    maj models.py

--- budget/models.py
+++ budget/models.py
@@ -25,9 +25,8 @@ class Project(models.Model):
         expense_amount = total_temp
         new_budget_temp = budget_temp - expense_amount
 
-
         for expense in expense_list:
-            total_expense_amount += expense_amount
+            total_expense_amount += expense.amount
 
         # temporary solution, because the form currently only allows integer amounts
         total_expense_amount = int(total_expense_amount)
